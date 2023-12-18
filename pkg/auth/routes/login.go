cb4edb78b7da7a9e3cfeb9f542abd732a07326d7
tinchourteaga <38136900+tinchourteaga@users.noreply.github.com>
2023-12-18 15:17:28 -0300

    login and register routes logic (auth)

--- pkg/auth/routes/login.go
+++ pkg/auth/routes/login.go
@@ -1,1 +1,36 @@
 package routes
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/tinchourteaga/go-grpc-api-gateway/pkg/auth/pb"
+)
+
+type LoginRequestBody struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+func Login(ctx *gin.Context, authSvcClient pb.AuthServiceClient) {
+	body := LoginRequestBody{}
+
+	if err := ctx.BindJSON(&body); err != nil {
+		ctx.AbortWithError(http.StatusBadRequest, err)
+		return
+	}
+
+	pbLoginReq := pb.LoginRequest{
+		Email:    body.Email,
+		Password: body.Password,
+	}
+
+	res, err := authSvcClient.Login(context.Background(), &pbLoginReq)
+	if err != nil {
+		ctx.AbortWithError(http.StatusBadGateway, err)
+		return
+	}
+
+	ctx.JSON(int(res.Status), &res)
+}
--- pkg/auth/routes/register.go
+++ pkg/auth/routes/register.go
@@ -1,1 +1,36 @@
 package routes
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/tinchourteaga/go-grpc-api-gateway/pkg/auth/pb"
+)
+
+type RegisterRequestBody struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+func Register(ctx *gin.Context, authSvcClient pb.AuthServiceClient) {
+	body := RegisterRequestBody{}
+
+	if err := ctx.BindJSON(&body); err != nil {
+		ctx.AbortWithError(http.StatusBadRequest, err)
+		return
+	}
+
+	pbRegisterReq := &pb.RegisterRequest{
+		Email:    body.Email,
+		Password: body.Password,
+	}
+
+	res, err := authSvcClient.Register(context.Background(), pbRegisterReq)
+	if err != nil {
+		ctx.AbortWithError(http.StatusBadGateway, err)
+		return
+	}
+
+	ctx.JSON(int(res.Status), &res)
+}

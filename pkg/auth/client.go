f478f0cedf0ef48958d70d8581b3578de8a6c25b
tinchourteaga <38136900+tinchourteaga@users.noreply.github.com>
2023-12-18 16:28:13 -0300

    client and middleware

--- pkg/auth/client.go
+++ pkg/auth/client.go
@@ -1,1 +1,22 @@
 package auth
+
+import (
+	"github.com/rs/zerolog/log"
+	"github.com/tinchourteaga/go-grpc-api-gateway/pkg/auth/pb"
+	"github.com/tinchourteaga/go-grpc-api-gateway/pkg/config"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+type AuthServiceClient struct {
+	Client pb.AuthServiceClient
+}
+
+func NewAuthServiceClient(c *config.Config) pb.AuthServiceClient {
+	connection, err := grpc.Dial(c.AuthSvcUrl, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Error().Msg("could not establish connection: " + err.Error())
+	}
+
+	return pb.NewAuthServiceClient(connection)
+}
--- pkg/auth/middleware.go
+++ pkg/auth/middleware.go
@@ -1,1 +1,47 @@
 package auth
+
+import (
+	"context"
+	"net/http"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+	"github.com/tinchourteaga/go-grpc-api-gateway/pkg/auth/pb"
+)
+
+type AuthMiddleware struct {
+	authSvcClient *AuthServiceClient
+}
+
+func NewAuthMiddleware(asc *AuthServiceClient) AuthMiddleware {
+	return AuthMiddleware{authSvcClient: asc}
+}
+
+func (am *AuthMiddleware) Authenticate(ctx *gin.Context) {
+	authorization := ctx.Request.Header.Get("authorization")
+
+	if authorization == "" {
+		ctx.AbortWithStatus(http.StatusUnauthorized)
+		return
+	}
+
+	token := strings.Split(authorization, "Bearer ")
+
+	if len(token) < 2 {
+		ctx.AbortWithStatus(http.StatusUnauthorized)
+		return
+	}
+
+	pbValidateReq := pb.ValidateRequest{
+		Token: token[1],
+	}
+
+	res, err := am.authSvcClient.Client.Validate(context.Background(), &pbValidateReq)
+	if err != nil || res.Status != http.StatusOK {
+		ctx.AbortWithStatus(http.StatusUnauthorized)
+		return
+	}
+
+	ctx.Set("userId", res.UserId)
+	ctx.Next()
+}
